ed0e3f7ef9be19c91172690b9b6c6f629856f214
Melanie Tran <melaniexx02@gmail.com>
2021-01-16 20:02:03 -0700

    Write test for setting api method

---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Users Api" do
+  it 'can create a new user' do
+    user_params = { 'email': 'email@gmail.com',
+      'password': 'password',
+      'password_confirmation': 'password' }
+
+    post '/api/v1/users', params: user_params
+    json = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(response).to be_successful
+    expect(json[:data][:attributes].keys).to_not eq([:id, :email, :api_key, :password])
+    expect(json[:data][:attributes].keys).to eq([:id, :email, :api_key])
+    expect(json[:data][:attributes][:email]).to eq('email@gmail.com')
+    expect(json[:data][:attributes][:api_key]).to_not be_nil
+  end
+end
