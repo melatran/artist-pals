f914ac7e578c8ec860ab6c121727071b9f78f671
Melanie Tran <melaniexx02@gmail.com>
2021-01-17 06:54:54 -0700

    Create and test merchant items

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
+      end
+
       resources :users, only: [:create]
       resources :merchants, only: [:index, :show]
       resources :items
