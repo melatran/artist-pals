99958ba1d3653ed35692e3ca55f1b47d9bb5f58f
Melanie Tran <melaniexx02@gmail.com>
2021-01-16 22:33:50 -0700

    Get all merchants route tested and created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
+      resources :merchants, except: [:new, :edit]
     end
   end
 end
