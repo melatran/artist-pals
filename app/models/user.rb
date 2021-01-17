f8244700f422e6ebad15fba707b5b4fb4755e081
Melanie Tran <melaniexx02@gmail.com>
2021-01-16 19:44:25 -0700

    Create method to set api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password_digest, require: true
+
+  has_secure_password
+
+  before_create :create_api_key
+
+  def create_api_key
+    self.api_key = SecureRandom.uuid
+  end
 end
