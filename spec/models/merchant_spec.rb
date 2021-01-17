4763e0a73fca2549f2403f2542bd5fab4ec69682
Melanie Tran <melaniexx02@gmail.com>
2021-01-16 21:43:38 -0700

    Write tests for model relationships

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Merchant do
+  describe "validations" do
+    it {should validate_presence_of :name}
+  end
+
+  describe "relationships" do
+    it {should have_many :invoices}
+    it {should have_many :items}
+    it {should have_many(:transactions).through(:invoices)}
+  end
+end
