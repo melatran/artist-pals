2698f131093246a53f40d71bd4582b3869aba175
Melanie Tran <melaniexx02@gmail.com>
2021-01-16 22:37:08 -0700

    Get one merchant

--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -10,4 +10,16 @@ describe "Merchants API" do
     merchants = JSON.parse(response.body)
     expect(merchants["data"].length).to eq(3)
   end
+
+  it "can get one merchant by id" do
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/#{merchant.id}"
+
+    merchant_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(merchant_json[:data][:id]).to eq(merchant.id.to_s)
+    expect(merchant_json[:data][:attributes][:name]).to eq(merchant.name)
+  end
 end
