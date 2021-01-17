fd21bd1098939d0d20a7b245e1bcf91eceb2e32d
Melanie Tran <melaniexx02@gmail.com>
2021-01-16 23:06:02 -0700

    Update an item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -47,4 +47,19 @@ describe "Items Api" do
     expect(item.photo).to eq(item_params[:photo])
     expect(item.merchant_id).to eq(item_params[:merchant_id])
   end
+
+  it 'can update an item' do
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+    previous = Item.last.name
+    item_params = {name: 'Mini Fan'}
+
+    patch "/api/v1/items/#{item.id}", params: item_params
+
+    item_object = Item.find_by(id: item.id)
+
+    expect(response).to be_successful
+    expect(item_object.name).to_not eq(previous)
+    expect(item_object.name).to eq('Mini Fan')
+  end
 end
