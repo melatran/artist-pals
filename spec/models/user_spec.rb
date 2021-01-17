0a9e46d73dc832d5f7597a5e8e504a8d85a7e0e2
Melanie Tran <melaniexx02@gmail.com>
2021-01-16 19:43:57 -0700

    Create test for creating a unique api key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,15 @@ describe User do
     it {should validate_uniqueness_of :email}
     it {should validate_presence_of :password_digest}
   end
+
+  describe "class methods" do
+    it "#create_api_key" do
+      user = User.create!(email: 'email@gamil.com', password: '1234', password_confirmation: '1234')
+
+      user.save
+
+      recent_user = User.last
+      expect(recent_user.api_key).to_not be_nil
+    end
+  end
 end
